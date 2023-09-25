4dcc630c273c7e945aa17903844043b05d76d71a
LemonDrop1228 <63515535+LemonDrop1228@users.noreply.github.com>
2023-09-25 16:08:25 +0000

    Update EasyInstall_Script.ps1
    Signed-off-by: LemonDrop1228 <63515535+LemonDrop1228@users.noreply.github.com>

--- EasyInstall_Script.ps1
+++ EasyInstall_Script.ps1
@@ -1,12 +1,12 @@
-﻿# Function to generate the command to be added
+# Function to generate the command to be added
 function addPathToEnvironmentVariableCommand ($appPath) {
     $messageBody = @"
-To add TerminalGPT.exe's root path to the system environment variables, please copy and paste the following command into your PowerShell:
+    To add TerminalGPT.exe's root path to the system environment variables, please copy and paste the following command into your PowerShell:
 
-    [Environment]::SetEnvironmentVariable("PATH",`$ENV:Path+";$appPath", [System.EnvironmentVariableTarget]::User)
+        [Environment]::SetEnvironmentVariable("PATH",`$ENV:Path+";$appPath", [System.EnvironmentVariableTarget]::User)
 
-This command adds TerminalGPT into the PATH system environment variables, allowing you to easily run TerminalGPT from any command prompt or PowerShell session without specifying the full path to TerminalGPT.exe.
-"@
+    This command adds TerminalGPT into the PATH system environment variables, allowing you to easily run TerminalGPT from any command prompt or PowerShell session without specifying the full path to TerminalGPT.exe.
+    "@
     Write-Output $messageBody
 }
 
@@ -14,8 +14,9 @@ This command adds TerminalGPT into the PATH system environment variables, allowi
 $client = New-Object System.Net.WebClient;
 
 # URL and file path setting
-$url = 'https://github.com/LemonDrop1228/Terminal.GPT/releases/latest/download/Terminal-GPT.zip';
+$url = 'https://github.com/LemonDrop1228/TerminalGPT/releases/latest/download/Terminal-GPT.zip';
 $file = "$env:TEMP\Terminal-GPT.zip";
+
 
 # Cleanup function
 function cleanupOnError() {
@@ -38,11 +39,13 @@ function cleanupOnError() {
     }
 }
 
-
+Write-Output "Attempting to download Terminal-GPT.zip from `n$url"
 try {
     $client.DownloadFile($url, $file);
+    Write-Output "`nDownload Successful. Proceeding to next step`n"
 } catch {
-    Write-Error "Download failed with the following exception: $_. Please make sure you're connected to the internet."
+    Write-Error "`nDownload Failed with the following exception: $_. Please make sure you're connected to the internet.`n"
+    Read-Host "`nPress Enter to clean up and exit the script"
     cleanupOnError
     exit
 }
@@ -51,14 +54,20 @@ $shell = new-object -com shell.application;
 $zip = $shell.NameSpace($file);
 $destination = "$env:LOCALAPPDATA\TerminalGPT"
 
-if(Test-Path $destination) { Remove-Item -Path $destination -Recurse }
+Write-Output "`nProcess to unzip and move the files will now start`n"
+if(Test-Path $destination) {
+    Write-Output "`nRemoving any previous files at $destination"
+    Remove-Item -Path $destination -Recurse
+}
 
 try {
     foreach($item in $zip.items()){
         $shell.Namespace($destination).copyhere($item);
     }
+    Write-Output "`nFile extraction Successful. Proceeding to next step"
 } catch {
-    Write-Error "Unzipping failed with the following exception: $_. Please ensure you have necessary privileges."
+    Write-Error "`nFailed to unzip files with the following exception: $_. Please ensure you have necessary privileges`n"
+    Read-Host "`nPress Enter to clean up and exit the script"
     cleanupOnError
     exit
 }
@@ -66,7 +75,11 @@ try {
 Remove-Item -Path $file;
 $shortcutLocation = "$env:USERPROFILE\Desktop\TerminalGPT.lnk";
 
-if(Test-Path -Path $shortcutLocation) { Remove-Item -Path $shortcutLocation }
+Write-Output "`nShortcut creation process will now start"
+if(Test-Path -Path $shortcutLocation) {
+    Write-Output "`nRemoving any previous shortcut at $shortcutLocation"
+    Remove-Item -Path $shortcutLocation
+}
 
 $WshShell = New-Object -comObject WScript.Shell;
 $Shortcut = $WshShell.CreateShortcut($shortcutLocation);
@@ -74,12 +87,14 @@ $Shortcut.TargetPath = "$destination\TerminalGPT.exe";
 
 try {
     $Shortcut.Save();
+    Write-Output "`nShortcut creation Successful."
 } catch {
-    Write-Error "Shortcut creation failed with the following exception: $_. Please make sure you're running this script as an Administrator."
+    Write-Error "`nShortcut creation failed with the following exception: $_. Please make sure you're running this script as an Administrator`n"
+    Read-Host "`nPress Enter to clean up and exit the script"
     cleanupOnError
     exit
 }
 
 # Final message
-Write-Output "Installation completed! A shortcut to TerminalGPT has been created on your Desktop."
+Write-Output "`nInstallation completed! A shortcut to TerminalGPT has been created on your Desktop."
 addPathToEnvironmentVariableCommand($destination)
